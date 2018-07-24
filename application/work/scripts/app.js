4b327395599e6e21d0bd1685f5cc5b149be64bce
Stephen Tramer <sttramer@microsoft.com>
2018-07-24 10:15:09 -0700

    Update app.js to use generic page advance method

--- application/work/scripts/app.js
+++ application/work/scripts/app.js
@@ -3,12 +3,19 @@
     'use strict';
 
     function uuidv4() {
-        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        )
-      }
+        if ('crypto' in window) {
+            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+            )
+        }
+        else {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
+    }
       
-      console.log(uuidv4());
     var app = {
         isLoading: true,
         spinner: document.querySelector('.loader'),
@@ -19,13 +26,18 @@
     };
 
     document.getElementById('startCheckin').addEventListener('click', function() {
-        app.startCheckinProcess();
+        app.loadNextPage('id', uuidv4(), 'question1.html');
     });
 
-    app.startCheckinProcess = function() {
-        var checkinId = uuidv4();
-        app.session.id = checkinId;
+    // Assign session key and advance
+    app.loadNextPage = function(key, value, page) {
+        app.session[key] = value;
+        window.location = page;
+    }
 
-        window.location = 'question1.html';
-    };
+    // Clear session and return to checkin
+    app.cancelCheckin = function() {
+        app.session = {};
+        window.location = 'checkin.html';
+    }
 })();
