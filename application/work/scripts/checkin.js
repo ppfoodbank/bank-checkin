64c86daaf0aeba880658ce1e95f1b5880cdc5a12
Swathi Jayavel <swjayav@microsoft.com>
2018-07-25 09:02:20 -0700

    Added localforage to project, send sessionData to SQL

--- application/work/scripts/checkin.js
+++ application/work/scripts/checkin.js
@@ -16,5 +16,10 @@
         navigator.serviceWorker
             .register('./service-worker.js')
             .then(function() { console.log('Service Worker Registered'); });
-        }
-    })();
+
+        // Then later, request syncs
+        navigator.serviceWorker.ready.then(function(swRegistration) {
+            return swRegistration.sync.register('syncDB');
+        });
+    }
+})();
