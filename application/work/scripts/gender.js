6810d585039a25ea3a7a2cebcb5c5a1b9ff84fd2
Holly Mitchell <homitch@microsoft.com>
2018-07-25 11:58:00 -0700

    Remove gender other input, just have the button

--- application/work/scripts/gender.js
+++ application/work/scripts/gender.js
@@ -2,7 +2,6 @@
     'use strict'
 
     var gender = '';
-    var otherButtonSelected = false;
 
     document.getElementById('butCancel').addEventListener('click', function() {
         app.cancelCheckin();
@@ -13,31 +12,18 @@
     });
 
     document.getElementById('genderNextButton').addEventListener('click', function() {
-        if (otherButtonSelected) {
-            gender =  document.getElementById('genderOther-input').value;
-        }
         app.loadNextPage('Gender', gender, 'lang.html');
     });
 
     document.getElementById('genderMaleButton').addEventListener('click', function() {
         gender = 'male';
-        otherButtonSelected = false;
     });
 
     document.getElementById('genderFemaleButton').addEventListener('click', function() {
         gender = 'female';
-        otherButtonSelected = false;
     });
 
     document.getElementById('genderOtherButton').addEventListener('click', function() {
-        otherButtonSelected = true;
-    });
-
-    document.getElementById('genderOther-input').addEventListener('focus', function() {
-        document.getElementById('genderOtherButton').classList.add('active');
-    });
-
-    document.getElementById('genderOther-input').addEventListener('blur', function() {
-        document.getElementById('genderOtherButton').classList.remove('active');
+        gender = 'other';
     });
 })();
