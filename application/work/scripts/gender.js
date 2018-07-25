28b4cd09edbc46d72742681ed9d36517e3e3bb5d
Holly Mitchell <homitch@microsoft.com>
2018-07-25 11:54:44 -0700

    Change styling and add in Simplified Chinese strings

--- application/work/scripts/gender.js
+++ application/work/scripts/gender.js
@@ -2,6 +2,7 @@
     'use strict'
 
     var gender = '';
+    var otherButtonSelected = false;
 
     document.getElementById('butCancel').addEventListener('click', function() {
         app.cancelCheckin();
@@ -12,14 +13,31 @@
     });
 
     document.getElementById('genderNextButton').addEventListener('click', function() {
+        if (otherButtonSelected) {
+            gender =  document.getElementById('genderOther-input').value;
+        }
         app.loadNextPage('Gender', gender, 'lang.html');
     });
 
     document.getElementById('genderMaleButton').addEventListener('click', function() {
         gender = 'male';
+        otherButtonSelected = false;
     });
 
     document.getElementById('genderFemaleButton').addEventListener('click', function() {
         gender = 'female';
+        otherButtonSelected = false;
+    });
+
+    document.getElementById('genderOtherButton').addEventListener('click', function() {
+        otherButtonSelected = true;
+    });
+
+    document.getElementById('genderOther-input').addEventListener('focus', function() {
+        document.getElementById('genderOtherButton').classList.add('active');
+    });
+
+    document.getElementById('genderOther-input').addEventListener('blur', function() {
+        document.getElementById('genderOtherButton').classList.remove('active');
     });
 })();
--- application/work/scripts/zipcode.js
+++ application/work/scripts/zipcode.js
@@ -2,6 +2,7 @@
     'use strict';
 
     var zipcode = -1;
+    var otherButtonSelected = false;
 
     document.getElementById('butCancel').addEventListener('click', function() {
         app.cancelCheckin();
@@ -13,17 +14,35 @@
 
     document.getElementById('zipcodeFirst').addEventListener('click', function(){
         zipcode = 98101;
+        otherButtonSelected = false;
     });
 
     document.getElementById('zipcodeSecond').addEventListener('click', function(){
         zipcode = 98121;
+        otherButtonSelected = false;
     });
 
     document.getElementById('zipcodeThird').addEventListener('click', function(){
         zipcode = 98104;
+        otherButtonSelected = false;
+    });
+
+    document.getElementById('zipcodeOther').addEventListener('click', function(){
+        otherButtonSelected = true;
+    });
+
+    document.getElementById('zipcodeOther-input').addEventListener('focus', function() {
+        document.getElementById('zipcodeOther').classList.add('active');
+    });
+x
+    document.getElementById('zipcodeOther-input').addEventListener('blur', function() {
+        document.getElementById('zipcodeOther').classList.remove('active');
     });
 
     document.getElementById('zipcodeNextButton').addEventListener('click', function(){
+        if (otherButtonSelected) {
+            zipcode = document.getElementById('zipcodeOther-input').value;
+        }
         app.loadNextPage('zipCode', zipcode, 'age.html');
     });
 })();
