d64b563e94025a5dfd7dd9b0b297e2d9df982a35
Bryce Johnson <t-brjohn@microsoft.com>
2018-07-25 10:30:43 -0700

    add radio button to ethnic page

--- application/work/scripts/ethnic.js
+++ application/work/scripts/ethnic.js
@@ -9,31 +9,42 @@
 
     //Selections
     document.getElementById('shl').addEventListener('click', function() {
+        this.classList.toggle('active');
         ethnicGroup = null;
     });
 
     document.getElementById('ian').addEventListener('click', function() {
+        this.classList.toggle('active');
         ethnicGroup = null;
     });
 
     document.getElementById('aaa').addEventListener('click', function() {
+        this.classList.toggle('active');
         ethnicGroup = null;
     });
 
     document.getElementById('bao').addEventListener('click', function() {
+        this.classList.toggle('active');
         ethnicGroup = null;
     });
 
     document.getElementById('nhpi').addEventListener('click', function() {
+        this.classList.toggle('active');
         ethnicGroup = null;
     });
 
     document.getElementById('wc').addEventListener('click', function() {
+        this.classList.toggle('active');
         ethnicGroup = null;
     });
 
+    //Other Text Field
     document.getElementById('o').addEventListener('click', function() {
-        ethnicGroup = document.getElementById('ethnic-input').nodeValue;
+        this.classList.toggle('active');
+    });
+
+    document.getElementById('ethnic-input').addEventListener('oninput', function() {
+        document.getElementById('o').classList.toggle('active');
     });
 
     //Navigation
@@ -41,8 +52,10 @@
         app.loadPreviousPage('family.html');
     });
 
-    // TODO change this to save the actual values instead of id
     document.getElementById('ethnicNext').addEventListener('click', function() {
+        if ((document.getElementById('ethnic-input').nodeValue).length > 0) {
+            ethnicGroup
+        }
         app.loadNextPage('ethnicGroups', ethnicGroup, 'gender.html');
     });
 })();
